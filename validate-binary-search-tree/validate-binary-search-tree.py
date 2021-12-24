0d586ccd97d0f321c5ad3523454d65a1ed92a5fe
Armanul Ambia <a.ambia9000@gmail.com>
2021-12-23 18:54:11 -0800

    Time: 40 ms (40.07%), Space: 18.1 MB (43.70%) - LeetHub

---
+++ validate-binary-search-tree/validate-binary-search-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        cur, stack, res = root, [], []
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+        # res is the inorder list
+        # now i just need to check if its increasing
+        return all([res[i]<res[i+1] for i in range(len(res)-1)])
+    
+        
