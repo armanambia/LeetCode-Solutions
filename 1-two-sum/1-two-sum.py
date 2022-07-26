eb21ec31ae3973af3bab8a568ce9ed52c94e381d
Armanul Ambia <arman714@csu.fullerton.edu>
2022-07-26 12:50:18 -0700

    Time: 52 ms (86.71%), Space: 14.5 MB (22.19%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        parsed = {}
+        for i,v in enumerate(nums):
+            if parsed.has_key(target-v):
+                return [i, parsed[target-v]]
+            else:
+                parsed[v] = i
+        
