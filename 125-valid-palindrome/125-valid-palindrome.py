c532a46a7ee52ee0a637cf09e7d406460fe1e15d
Armanul Ambia <arman714@csu.fullerton.edu>
2022-07-29 23:22:51 -0700

    Time: 55 ms (67.66%), Space: 14.7 MB (49.27%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -6,4 +6,15 @@ class Solution(object):
         """
         res = ''.join(c for c in s if c.isalnum())
         res = res.lower()
-        return res == res[::-1]
+        f = 0
+        b = len(res) - 1
+        while f < b:
+            if res[f] != res[b]:
+                return False
+            f += 1
+            b -= 1
+        if b == f:
+            return res[b] == res[f]
+        else:
+            return True
+            
