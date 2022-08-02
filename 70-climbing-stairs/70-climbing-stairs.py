7f9b0694cd6793c8e86fac9e81e03c308989f1f2
Armanul Ambia <arman714@csu.fullerton.edu>
2022-08-01 22:23:58 -0700

    Time: 25 ms (53.32%), Space: 13.6 MB (12.43%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 3:
+            return n
+        i = 4
+        f = 2
+        s = 3
+
+        # Find & Displaying
+        
+        while(i <= n):
+            # Next = f + s
+            # f = s
+            # s = Next
+            f, s = s, f+s
+            i += 1
+        
+        return s
