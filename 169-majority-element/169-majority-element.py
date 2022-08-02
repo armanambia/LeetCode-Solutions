6c2e7c80329ba9eb895d8a3750cad9945493a476
Armanul Ambia <arman714@csu.fullerton.edu>
2022-08-02 01:24:11 -0700

    Time: 235 ms (33.22%), Space: 14.8 MB (52.03%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,12 +1,11 @@
 class Solution(object):
     def majorityElement(self, nums):
-        freq_map = {}
-        freq = 0
-        val = 0
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        fmap = {}
         for x in nums:
-            freq_map[x] = freq_map.get(x, 0) + 1
-            temp = freq_map[x]
-            if temp > freq:
-                freq = temp
-                val = x
-        return val
+            fmap[x] = fmap.get(x,0) + 1
+            if fmap[x] > len(nums)/2:
+                return x
