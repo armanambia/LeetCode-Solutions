340ce6b6645e1c0385d62ee016fdc1a8e23e63ff
Armanul Ambia <arman714@csu.fullerton.edu>
2022-08-01 21:44:46 -0700

    Time: 39 ms (6.29%), Space: 13.5 MB (33.74%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -11,10 +11,10 @@ class Solution(object):
         """
         l = 1
         r = n
-        while l <= r:
+        while l < r:
             mid = (l + r ) // 2
             if isBadVersion(mid):
-                r = mid -1
+                r = mid
             else:
                 l = mid + 1
         return l
