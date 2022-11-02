af3cef5fded795724a4e58ac37e4b9a74ba1b6b5
Armanul Ambia <arman714@csu.fullerton.edu>
2022-11-01 22:25:56 -0700

    Time: 139 ms (13.96%), Space: 13.9 MB (64.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,14 +5,13 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        if len(s) != len(t):
+            return False
         hash_map1 = {}
         for x in s:
             hash_map1[x] = hash_map1.get(x, 0) + 1
         for x in t:
             hash_map1[x] = hash_map1.get(x, 0) - 1
-            if hash_map1[x] < 0:
-                return False
-        for value in hash_map1.values():
-            if value > 0: 
+            if hash_map1[x] == None or hash_map1[x] < 0:
                 return False
         return True
